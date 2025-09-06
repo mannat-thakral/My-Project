b59fe5b1b2b51b4855810a8d35db13fc626d5c7a
mannat-thakral <mannatthakral17@gmail.com>
2025-09-06 22:38:49 +0530

    Add files via upload

---
+++ sih.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  const errorBox = document.getElementById("error");
+
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+
+    // Demo credentials
+    if (username === "admin" && password === "1234") {
+      window.location.href = "mainpage.html"; // redirect
+    } else {
+      errorBox.textContent = "Invalid Username or Password";
+    }
+    
+  });
+});
